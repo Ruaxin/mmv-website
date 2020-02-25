0994ee5e8f751291f2852e48a5978b4d478ff017
ruaxin <qy.xin@foxmail.com>
2020-02-25 23:04:00 +0800

    deploy

---
+++ precache-manifest.dfdf9cc8c29bd63d2318fa0e7af836f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5daa602c829e1a637785",
+    "url": "/mmv-website/css/app.91ab15b8.css"
+  },
+  {
+    "revision": "32522c5b08b4f54f7ea8d6eb581afff4",
+    "url": "/mmv-website/index.html"
+  },
+  {
+    "revision": "5daa602c829e1a637785",
+    "url": "/mmv-website/js/app.ba2d84a2.js"
+  },
+  {
+    "revision": "43b0e175d158ec888ed2",
+    "url": "/mmv-website/js/chunk-vendors.2ba44f86.js"
+  },
+  {
+    "revision": "ddcb4e77e9d1bfc8a01ea23127185cce",
+    "url": "/mmv-website/manifest.json"
+  },
+  {
+    "revision": "35d8adce267c3125048d52e0153a5a3c",
+    "url": "/mmv-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/mmv-website/robots.txt"
+  }
+]);
